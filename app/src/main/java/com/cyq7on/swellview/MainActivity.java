c433e14eac6b99f1f3cacf4f379e1cfe74cf400d
cyq7on <cyq7on@qq.com>
2017-02-27 18:39:08 +0800

    first commit

---
+++ app/src/main/java/com/cyq7on/swellview/MainActivity.java
@@ -0,0 +1,49 @@
+package com.cyq7on.swellview;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements Runnable {
+
+    private SwellView swellView;
+    private int percent = 0;
+    private static final int delayTime = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                percent = 0;
+                swellView.reset();
+            }
+        });
+
+        swellView = (SwellView) findViewById(R.id.swellView);
+        swellView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                swellView.setSubtitleText("开始");
+                swellView.postDelayed(MainActivity.this, delayTime);
+            }
+        });
+    }
+
+    @Override
+    public void run() {
+        if (percent <= 100) {
+            swellView.setTitleText(percent++ + "%");
+            swellView.swell(0.01f,0.01f,delayTime);
+            swellView.postDelayed(MainActivity.this, delayTime);
+        }
+    }
+}
